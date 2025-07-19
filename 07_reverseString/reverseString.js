ce230d9f800966f6000dfbcbe57646b8d4eb50b4
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-07-18 19:58:50 -0500

    Completed exercise 7: reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let characters = string.split("");
+    for (let i = 0; i < characters.length / 2; i++) {
+        let temp = characters[i];
+        characters[i] = characters[characters.length - 1 - i];
+        characters[characters.length - 1 - i] = temp;
+    }
+    return characters.join("");
 };
 
 // Do not edit below this line
