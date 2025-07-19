554d3b2dd9c61159e2177ce205616c93491b7244
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-07-18 19:52:44 -0500

    Completed exercise 6: repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0)
+        return "ERROR";
+    let answer = "";
+    for (let i = 0; i < num; i++)
+        answer += string;
+    return answer;
 };
 
 // Do not edit below this line
