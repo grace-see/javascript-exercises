f4d0565890920836fcc13809ee96b954285294d1
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-07-22 21:32:51 -0500

    Completed exercise 9: sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0)
+        return "ERROR";
+    if (start % 1 !== 0 || end % 1 !== 0)
+        return "ERROR";
+    if (typeof start !== "number" || typeof end !== "number")
+        return "ERROR";
+    let sum = 0;
+    if (start < end) {
+        for (let i = start; i <= end; i++)
+            sum += i;
+    }
+    else {
+        for (let i = start; i >= end; i--)
+            sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
