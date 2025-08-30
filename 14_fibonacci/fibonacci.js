4724deaa27ad2664ee5e540978fa6cb0f0296ac4
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-08-29 23:27:31 -0500

    Completed exercise 14: fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0)
+        return "OOPS";
+    let first = 1;
+    let second = 1;
+    let third = 0;
+    if (num == 1)
+        return first;
+    if (num == 2)
+        return second;
+    for (let i = 2; i < num; i++) {
+        third = first + second;
+        first = second;
+        second = third;
+    }
+    return third;
 };
 
 // Do not edit below this line
