69127ca0134bc71003b684606783ca1b5c959a15
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-08-29 23:03:34 -0500

    Completed exercise 12: calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for (let i = 0; i < nums.length; i++)
+    sum += nums[i];
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let sum = nums[0];
+	for (let i = 1; i < nums.length; i++)
+    sum *= nums[i];
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let answer = num1;
+  for (let i = 1; i < num2; i++)
+    answer *= num1;
+	return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let answer = 1;
+  for (let i = 1; i <= num; i++)
+    answer *= i;
+  return answer;
 };
 
 // Do not edit below this line
