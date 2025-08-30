a5c96b4a84b510511812d1c2acd5135eee2c2a3d
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-08-29 23:20:17 -0500

    Completed exercise 13: palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replaceAll(".", "");
+    word = word.replaceAll("?", "");
+    word = word.replaceAll("!", "");
+    word = word.replaceAll(", ", "");
+    word = word.replaceAll(" ", "");
+    console.log(word);
+    for (let i = 0; i < word.length / 2; i++)
+        if (word[i].toLowerCase() !== word[word.length - 1 - i].toLowerCase())
+            return false;
+    return true;
 };
 
 // Do not edit below this line
