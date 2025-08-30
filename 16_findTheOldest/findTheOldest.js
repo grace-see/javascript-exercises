abc25b2dbe10beb71c9c975a9d475e8e3666e36e
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-08-30 00:41:04 -0500

    Completed exercise 16: findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let maxAge = (oldest.yearOfDeath === undefined) ? (new Date().getFullYear()) - oldest.yearOfBirth : oldest.yearOfDeath - oldest.yearOfBirth;
+    for (let i = 1; i < people.length; i++) {
+        let age = (people[i].yearOfDeath === undefined) ? (new Date().getFullYear()) - people[i].yearOfBirth : people[i].yearOfDeath - people[i].yearOfBirth;
+        if (age > maxAge) {
+            oldest = people[i];
+            maxAge = oldest.age;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
